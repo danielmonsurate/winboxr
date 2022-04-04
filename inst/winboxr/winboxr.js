7f3b6b62ae58f91952b40474ef85a27aee133ad3
danielmonsurate <37372423+danielmonsurate@users.noreply.github.com>
2022-04-04 18:24:34 +0100

    Update winboxr.js

--- inst/winboxr/winboxr.js
+++ inst/winboxr/winboxr.js
@@ -18,7 +18,7 @@ $(function () {
         bottom: data.bottom,
         left: data.left,
         html: data.html,
-        mount: document.querySelector(data.mount).cloneNode(true),
+        mount: if(data.mount) {document.querySelector(data.mount).cloneNode(true)} else {null},
         onresize: function(){
           var p = this.body.querySelector(".plotly");
           if(p) {
